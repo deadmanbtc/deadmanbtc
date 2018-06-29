2cc01db6acef4155c60f5758295aac888556f45e
Damian <damivazbien@gmail.com>
2018-06-29 23:57:21 +0100

    add resgister method

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,11 @@
 var appRouter = function (app) {
-    app.get("/register", function(req, res) {
+    app.get("/", function(req, res) {
       res.status(200).send("Welcome to our restful API");
+    });
+
+    //receive information from form
+    app.post("/register", function(req, res) {
+      res.send(req.body.publickey);
     });
   }
   
