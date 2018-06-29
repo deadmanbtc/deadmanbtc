57e1e001ea13d26d836b946970ce456dd2944434
Simão Cardoso <simaovalecardoso@gmail.com>
2018-06-30 00:19:10 +0100

    getBlock and gitignore

--- api/app.js
+++ api/app.js
@@ -3,11 +3,22 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 
+var request = require('request');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
     console.log("app running on port.", server.address().port);
 });
+
+
+
+app.getBlock = function getBlock(blockNo) {
+    request('https://blockchain.info/rawblock/' + blockNo, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(body);
+    });
+}
