baf80ae1ec31f5e792de35646029e5593649e715
Simão Cardoso <simaovalecardoso@gmail.com>
2018-06-30 15:15:54 +0100

    sendData function

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ var server = app.listen(3000, function() {
 });
 
 
-app.getBlock = function getBlock(blockNo) {
+app.getBlock = function (blockNo) {
     request('https://blockchain.info/rawblock/' + blockNo, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         console.log(body);
@@ -24,23 +24,41 @@ app.getBlock = function getBlock(blockNo) {
 }
 
 
-app.example = async function example() {
+app.sendData = async function(ins, data) {
 
     var cla = 0x80;
-    var ins = 0x02;
+
+    data = Buffer.from(data, "utf8");
+
+    var offset = 0;
     var p1 = 0x80;
-    var p2 = 0;
-    var data = "Hello";
-    // [0x154,1,180,0x12,0x15,....]
-    var buffer = Buffer.from(data, "utf8");
+    var p2 = 0x00;
+    var chunk = "";
 
     const transport = await TransportNodeHid.default.create(5000);
-    transport.setDebugMode(true);
+    //transport.setDebugMode(true);
 
-    transport.send(cla, ins, p1, p2, buffer).then(response => {
-        console.log("response: " + JSON.stringify(response));
-    });
+    while (offset !== data.length) {
+        if (data.length - offset > 255)
+            chunk = data.slice(offset, offset + 255);
+        else
+            chunk = data.slice(offset);
+
+        if (offset + chunk.length === data.length)
+            p1 = 0x80;
+        else
+            p1 = 0x00;
+
+        await transport.send(cla, ins, p1, p2, chunk).then(response => {
+            //TODO do something with the response
+
+            console.log("response: " + JSON.stringify(response));
+        });
+
+        offset += chunk.length;
+    }
+    // [0x154,1,180,0x12,0x15,....]
 
 }
 
-app.example();
+app.sendData(0x02, "hello");
